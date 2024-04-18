f8cec392fa12133751ade04d996aeb5363b6c0e6
Reynolds <salesrcool@gmail.com>
2024-04-18 16:00:39 -0400

    first attempt

---
+++ week1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace week1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 1;
+		while (i<101)
+		{
+			if(i%3==0 && i%5 ==0)
+			{
+				Console.WriteLine("fizzbuzz");
+			}
+			else if(i%5==0)
+			{
+					Console.WriteLine("buzz");
+			}
+			else if(i%3 == 0)
+			{
+				Console.WriteLine("fizz");
+			}
+			else Console.WriteLine(i);
+			i+= 1;
+		}
+    }
+}
